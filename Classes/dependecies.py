241b68328ee213e905248a47667f7ef4847527d3
Moy <navam9897@gmail.com>
2024-08-19 22:25:03 -0600

    Dependencies combined

---
+++ Classes/dependecies.py
@@ -0,0 +1,6 @@
+import Classes.backtest as BackTest
+import Classes.randomForest as RandomForest
+import Classes.data_operations as DataOperations
+import Classes.Strategies as Strategies
+from Classes.MT5 import MT5
+from Classes.technical import Technical

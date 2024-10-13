fd47fff649d9e94b41bca474a3137ba71bd8d546
Moy <navam9897@gmail.com>
2024-10-13 11:36:57 -0600

    REFACTOR: Add docs and regactor PARAMETERS to a dict

--- Classes/Strategies.py
+++ Classes/Strategies.py
@@ -8,15 +8,15 @@ from Classes.randomForest import inputs_for_random_forest,get_prediction,inputs_
 from threading import Timer
 
 # Parameteters for strategy (not modify)
-OFFSET = 2
-CHOP_LIMIT = 50.24
-CHOP_LENGHT = 4
-ATR_LENGTH = 15
-FACTOR = 2.4
-EMA_OPEN_EURUSD = 2
-EMA_LH_EURUSD = 2
-EMA_OPEN_XAUUSD = 4
-EMA_LH_XAUUSD = 2
+PARAMETERS = lambda x: {    
+        "OFFSET": 2,
+        "CHOP_LIMIT": 50.24,
+        "CHOP_LENGHT": 4,
+        "ATR_LENGHT": 15,
+        "FACTOR": 2.4,
+        "EMA_OPEN": 4 if x == "XAUUSD" else 2,
+        "EMA_LH": 2
+    }
 
 # DEFAULT CUSTOM PARAMETERS
 TARGET_POINTS_EURUSD = 65
@@ -62,9 +62,7 @@ def single_trade_open(object,conn,symbol_to_trade,partial_close,risk,target_prof
     id = 0
     date_for_df = str(date.today())
     fibonacci_depth = 10
-    fibonacci_levels = dict()
-    FINAL_EMA_OPEN = EMA_OPEN_XAUUSD if symbol_to_trade == "XAUUSD" else EMA_OPEN_EURUSD
-    FINAL_EMA_LH = EMA_LH_XAUUSD if symbol_to_trade == "XAUUSD" else EMA_LH_EURUSD
+    fibonacci_levels = dict()    
     max_profit_trades = int(.6 * max_trades) if max_trades != 1 else 1
     max_loss_trades = int(.4 * max_trades) if max_trades > 2 else 1
     total_profit = 0
@@ -155,7 +153,7 @@ def single_trade_open(object,conn,symbol_to_trade,partial_close,risk,target_prof
         # Avoid open positions when the profit/risk was acheived
         if (total_profit <= target_profit) or (total_profit >= risk):
             # Open positions if the stratgy detects entries            
-            position, entry = EMA_CROSSING(df=M1,offset= OFFSET, ema_open=FINAL_EMA_OPEN,ema_period= FINAL_EMA_LH,reverse=False)  
+            position, entry = EMA_CROSSING(df=M1,offset= PARAMETERS(symbol_to_trade)["OFFSET"], ema_open=PARAMETERS(symbol_to_trade)["EMA_OPEN"],ema_period= PARAMETERS(symbol_to_trade)["EMA_LH"],reverse=True)  
             if randomForest:
                 data = inputs_for_random_forest_v2(M1,symbol_to_trade,points_value)  
                 entry_from_model = get_prediction(data,'predict_entries_model')      
@@ -241,102 +239,27 @@ def single_trade_open(object,conn,symbol_to_trade,partial_close,risk,target_prof
         else:
             pd.concat(TRADES_SIGNALS).to_csv(fr"C:\Users\Moy\Documents\Python\Algorithmic Trading\HFT\backtest_data\{symbol_to_trade}-{date_for_df}-{points}-{id}.csv")     
 
-def multiple_entries_open(object,conn,symbol_to_trade,risk,target_profit,entries_per_trade,max_trades,timeFrame,flag_session,flag_position,points,lots,min=10):
-    """
-        Strategy open multiples trades and monitor profit until sessions is closed. Strategy wait 10 min by default to open a new trade in case a signal is detected        
-    """                
-    def reset_timer():       
-        nonlocal timer_active
-        timer_active = False
-        print("Timer end, trades can be opened again.") 
-        
-    min_between_trades = min * 60
-    open_trades = True
-    trades = 0    
-    TRADES_SIGNALS = []    
-    id = 0
-    date_for_df = str(date.today())    
-    FINAL_EMA_OPEN = EMA_OPEN_XAUUSD if symbol_to_trade == "XAUUSD" else EMA_OPEN_EURUSD
-    FINAL_EMA_LH = EMA_LH_XAUUSD if symbol_to_trade == "XAUUSD" else EMA_LH_EURUSD    
-    total_profit = 0            
-    balance = conn.account_details().balance        
-    risk = (balance * risk) * -1
-    target_profit = balance * target_profit
-    timer_active = False    
-    while not flag_session.is_set():                
-        # Close operations if the button from the GUI is pressed
-        if positions_open(conn,symbol_to_trade) and flag_position.is_set():  
-            tickets_to_close = conn.get_positions().ticket.values          
-            print(f"Next Trades wil be closed: {tickets_to_close}")
-            for ticket in tickets_to_close:                
-                conn.close_position(symbol_to_trade, ticket, entry, lots, comment="Closed by limit reached")                
-            check_balance = True
-            # Clear the flag to avoid close the next entries
-            flag_position.clear()                           
-                                                                   
-        # Close the session if profit/loss or max entries was reached
-        if (total_profit >= target_profit) or (total_profit <= risk):
-            print(f"The session was closed automatically by {'loss' if total_profit < 0 else 'profit'} limit reached!")
-            break
-        if trades >= max_trades:
-            print("Maximun trades reached, no more trades will be opened")
-            open_trades = False                  
-        try:            
-            M1 = conn.get_data(symbol_to_trade, timeFrame, 100)
-        except Exception as e:
-            print("Data range failed:", e)
-            break
-        # Open trades while limits are not reached
-        if open_trades and not timer_active and total_profit <= target_profit and total_profit >= risk:
-            # Open positions if the stratgy detects entries            
-            position, entry = EMA_CROSSING(df=M1,offset= OFFSET, ema_open=FINAL_EMA_OPEN,ema_period= FINAL_EMA_LH,reverse=False)  
-            if position:                                        
-                print("******************", "BUY" if entry == 1 else "SELL", "******************")                
-                tickets = [] 
-                tickets_copy = []                                 
-                while len(tickets) < entries_per_trade:                                        
-                    ticket = conn.open_position(symbol_to_trade, entry, lots,points)
-                    if ticket != 0:
-                        tickets.append(ticket) 
-                valid_entries = False
-                for ticket in tickets:
-                    if ticket != 10019:
-                        valid_entries = True
-                        tickets_copy.append(ticket)                        
-                if not valid_entries:
-                    flag_session.set()
-                    print("Please reduce the lots")  
-                else:                                 
-                    trades += 1  
-                    # Start timer to prevent more trades for 10 min
-                    timer_active = True
-                    Timer(min_between_trades, reset_timer).start()
-        sleep(1)
-    # Close all trades before close the session
-    if positions_open(conn,s=symbol_to_trade):        
-        tickets_to_close = conn.get_positions().ticket.values          
-        print(f"Next Trades wil be closed before close the session: {tickets_to_close}")
-        for ticket in tickets_to_close:                
-            conn.close_position(symbol_to_trade, ticket, entry, lots, comment="Close Trades")
-    if not flag_session.is_set():                  
-        flag_session.set()
-    # Display the buttons when session is closed automatically by one condtion reached
-    try:        
-        object.main_frame.stop_thread.grid_forget()    
-        object.main_frame.close_trades.grid_forget()                
-        object.sidebar_button_2.configure(state="enabled")
-        object.main_frame.stop_thread = customtkinter.CTkButton(object.main_frame, text="Return Strategy Screen",command= object.start_connection)
-        object.main_frame.stop_thread.grid(row=7, column=0,columnspan=2, padx=40, pady=0,sticky="ew")                             
-    except:
-        pass
-    # Export the current operations
-    print(f"Profit: {total_profit}")  
-    if len(TRADES_SIGNALS) > 0:        
-        pd.concat(TRADES_SIGNALS).to_csv(fr"C:\Users\Moy\Documents\Python\Algorithmic Trading\HFT\backtest_data\{symbol_to_trade}-{date_for_df}-{points}-{id}.csv")     
-
 def EMA_CROSSING(df,offset=3, ema_open=15, ema_period=3,reverse=False,volume_filter=False,show=False):
     """
-        Check for crossing signals based on the parameters passed
+    Description:
+        The strategy looks for crossing between the upper and lower EMA with the central one. 
+        - CHOP Index < 50.
+        - Supertrend Direction is opposite to the signal detected by EMA Crossing.
+        - Trend of last 30 times is at least 35 % of signal detected. If not open in main trend     
+    Additional:
+        - Volume Filter: Volume increase from previous bar. (If not signal skipped)       
+
+    Args:
+        df (pandas dataframe): Data used to check for entries
+        offset (int, optional): _description_. Defaults to 3.
+        ema_open (int, optional): _description_. Defaults to 15.
+        ema_period (int, optional): _description_. Defaults to 3.
+        reverse (bool, optional): _description_. Defaults to False.
+        volume_filter (bool, optional): Extra Filter, If volume don't increase signal turns to False . Defaults to False.
+        show (bool, optional): Display signal detected to the console. Defaults to False.
+
+    Returns:
+        list --> boolean,int: Returns TRUE or FALSE and the trend to open trade
     """
     
     operation = False
@@ -348,12 +271,12 @@ def EMA_CROSSING(df,offset=3, ema_open=15, ema_period=3,reverse=False,volume_fil
         total = 30 - counters[2]
     else:
         total = 30    
-    if M1_technical.calculate_chopiness_index(CHOP_LENGHT)[-1] < CHOP_LIMIT:                
+    if M1_technical.calculate_chopiness_index(PARAMETERS("XAUUSD")["CHOP_LENGHT"])[-1] < PARAMETERS("XAUUSD")["CHOP_LIMIT"]:                
         # Set PARAMETERS         
         EMA_LOW = M1_technical.EMA(entry="low", period=ema_period, deviation=offset)
         EMA_HIGH = M1_technical.EMA(entry="high", period=ema_period, deviation=offset)
         EMA_OPEN = M1_technical.EMA(entry="open", period=ema_open, deviation=-1)
-        supertrend = M1_technical.calculate_super_trend(ATR_LENGTH, FACTOR)          
+        supertrend = M1_technical.calculate_super_trend(PARAMETERS("XAUUSD")["ATR_LENGHT"], PARAMETERS("XAUUSD")["FACTOR"])          
         if (CROSSING(EMA_OPEN, EMA_HIGH, 0)) and supertrend == 1:            
             # SELL Under
             if counters[-1] / total >= .35:
